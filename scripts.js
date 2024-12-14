5e354e36a755b3583d55905b37d57e1906049e73
SWIFTGI <144988246+SWIFTGI@users.noreply.github.com>
2024-12-14 12:44:29 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 var teamOnePoints = 0;
 var teamTwoPoints = 0;
+var darkModeOn = false;
+
+document.getElementById("showDarkModeToggle").innerHTML = 'Off';
 
 function addPointToTeamOne() {
     teamOnePoints += 1;
@@ -9,4 +12,42 @@ function addPointToTeamOne() {
 function addPointToTeamTwo() {
     teamTwoPoints += 1;
     document.getElementById("showPointsTwo").innerHTML = teamTwoPoints;
+}
+
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    if (darkModeOn == true) {
+        darkModeOn = false;
+        document.getElementById("showDarkModeToggle").innerHTML = 'Off';
+    } else {
+        darkModeOn = true;
+        document.getElementById("showDarkModeToggle").innerHTML = 'On';
+    }
+}
+
+function changeNameTeamOne() {
+    var inputValue = document.getElementById("textInputOne").value;
+    var container = document.getElementById("inputContainerOne");
+    var teamName = document.getElementById("teamNameOne");
+
+    if (inputValue.trim() !== "") {
+        teamName.innerText = inputValue;
+        container.style.display = "none";
+    } else {
+        alert("Please enter some text.");
+    }
+}
+
+function changeNameTeamTwo() {
+    var inputValue = document.getElementById("textInputTwo").value;
+    var container = document.getElementById("inputContainerTwo");
+    var teamName = document.getElementById("teamNameTwo");
+
+    if (inputValue.trim() !== "") {
+        teamName.innerText = inputValue;
+        container.style.display = "none";
+    } else {
+        alert("Please enter some text.");
+    }
 }
